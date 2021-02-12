7414c8bd0e164eb31ae8a46dde3a4d0e42a7b123
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-12 17:54:09 +0900

    Fix hard tabs in docker-compose.yml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/a2ikm/docker-compose-yml-init
 
 go 1.15
+
+require github.com/goccy/go-yaml v1.8.8
--- main.go
+++ main.go
@@ -3,25 +3,44 @@ package main
 import (
 	"log"
 	"os"
+
+	yaml "github.com/goccy/go-yaml"
 )
 
+type Service struct {
+	Image       string
+	Environment map[string]string
+}
+
+type Document struct {
+	Version  string
+	Services map[string]Service
+}
+
 func main() {
-	yml := `version: "3.8"
-services:
-	mysql:
-		image: mysql:8.0
-		environment:
-			MYSQL_DATABASE: app_development
-			MYSQL_USER: app
-			MYSQL_PASSWORD: password
-	`
+	doc := Document{
+		Version:  "3.8",
+		Services: map[string]Service{},
+	}
+	doc.Services["mysql"] = Service{
+		Image: "mysql:8.0",
+		Environment: map[string]string{
+			"MYSQL_DATABASE": "app_development",
+			"MYSQL_USER":     "app",
+			"MYSQL_PASSWORD": "password",
+		},
+	}
+
+	yml, err := yaml.Marshal(doc)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	f, err := os.OpenFile("docker-compose.yml", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer f.Close()
 
-	f.WriteString(yml)
+	f.Write(yml)
 }
