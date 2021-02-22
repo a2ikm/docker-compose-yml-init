b99b86b86540308d270b9f6e0b8d7b5cc6079b66
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-23 00:14:03 +0900

    Add postgres

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	yaml "github.com/goccy/go-yaml"
 )
 
+// Service wraps each service
 type Service struct {
 	Image       string
 	Environment map[string]string
 }
 
+// Document wraps whole YAML
 type Document struct {
 	Version  string
 	Services map[string]Service
@@ -19,15 +21,23 @@ type Document struct {
 
 func main() {
 	doc := Document{
-		Version:  "3.8",
-		Services: map[string]Service{},
-	}
-	doc.Services["mysql"] = Service{
-		Image: "mysql:8.0",
-		Environment: map[string]string{
-			"MYSQL_DATABASE": "app_development",
-			"MYSQL_USER":     "app",
-			"MYSQL_PASSWORD": "password",
+		Version: "3.8",
+		Services: map[string]Service{
+			"mysql": {
+				Image: "mysql:8.0",
+				Environment: map[string]string{
+					"MYSQL_DATABASE": "app_development",
+					"MYSQL_USER":     "app",
+					"MYSQL_PASSWORD": "password",
+				},
+			},
+			"postgres": {
+				Image: "postgres:13.2-alpine",
+				Environment: map[string]string{
+					"POSTGRES_USER":     "app",
+					"POSTGRES_PASSWORD": "password",
+				},
+			},
 		},
 	}
 
